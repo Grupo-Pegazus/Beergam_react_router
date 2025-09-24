c12a6bac1be091473d9c20d8b744d4d7c2b16002
Pegdevcolab02 <pegdevcolab02@gmail.com>
2025-09-24 17:01:11 -0300

    feat: adicionando efeitos de partículas na página de registro e login

--- app/features/auth/components/PageLayout/PageLayout.tsx
+++ app/features/auth/components/PageLayout/PageLayout.tsx
@@ -1,3 +1,7 @@
+import { loadAll } from "@tsparticles/all";
+import type { Container, ISourceOptions } from "@tsparticles/engine";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useMemo, useState } from "react";
 import Svg from "~/src/assets/svgs";
 import calendar from "~/src/img/auth/calendar.webp";
 import card from "~/src/img/auth/card.webp";
@@ -33,56 +37,189 @@ export default function PageLayout({
   children: React.ReactNode;
   pageType?: TPageType;
 }) {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadAll(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+  const options: ISourceOptions = useMemo(
+    () => ({
+      particles: {
+        number: {
+          value: 160,
+          density: {
+            enable: true,
+            valueArea: 800,
+          },
+        },
+        color: {
+          value: "#ffffff",
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#000000",
+          },
+          polygon: {
+            nb_sides: 5,
+          },
+          image: {
+            src: "img/github.svg",
+            width: 100,
+            height: 100,
+          },
+        },
+        opacity: {
+          value: 0.8,
+          ramdom: true,
+          animation: {
+            mode: "random",
+            enable: true,
+            startValue: "min",
+            decay: 0,
+            speed: 0.5,
+          },
+        },
+        size: {
+          value: 3,
+          animation: {
+            mode: "random",
+            enable: true,
+            startValue: "min",
+            decay: 0,
+            speed: 0.5,
+          },
+        },
+        lineLinked: {
+          enable: false,
+          distance: 150,
+          color: "#ffffff",
+          opacity: 0.4,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          speed: 1,
+          direction: "none",
+          random: true,
+          straight: false,
+          outModes: "out",
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600,
+          },
+        },
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onHover: {
+            enable: true,
+            mode: "bubble",
+          },
+          onclick: {
+            enable: true,
+            mode: "repulse",
+          },
+          resize: {
+            delay: 0,
+            enable: true,
+          },
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            lineLinked: {
+              opacity: 1,
+            },
+          },
+          bubble: {
+            distance: 250,
+            size: 0,
+            duration: 2,
+            opacity: 0,
+            speed: 3,
+          },
+          repulse: {
+            distance: 400,
+            duration: 0.4,
+          },
+          push: {
+            particles_nb: 4,
+          },
+          remove: {
+            particlesNb: 2,
+          },
+        },
+      },
+      retina_detect: true,
+    }),
+    []
+  );
   return (
-    <div className="flex  h-screen bg-gradient-to-b from-beergam-orange-light to-beergam-orange to-65%">
-      <data className="absolute top-2 left-2 w-30 h-30">
-        <img
-          src={beergam_flower_logo}
-          alt="beergam_flower_logo"
-          className="w-full h-full object-contain"
-        />
-      </data>
-      <div className="absolute top-0 left-0 max-w-screen max-h-screen overflow-hidden w-full h-full opacity-50">
-        <div className="absolute top-0 left-0 w-3/4 max-w-6xl object-contain">
+    <>
+      <div className="flex  h-screen bg-beergam-orange">
+        <data className="absolute top-2 left-2 w-30 h-30">
           <img
-            src={world_bg}
-            alt="world_bg"
-            className="w-full h-full max-w-screen-xl max-h-screen-2xl object-cover"
+            src={beergam_flower_logo}
+            alt="beergam_flower_logo"
+            className="w-full h-full object-contain"
           />
-        </div>
-        <div className="absolute max-h-[60rem] opacity-0 max-w-80 w-[16%] h-10/12 right-0 top-1/2 -translate-y-1/2 sm:opacity-100">
-          <div className="absolute right-[-30%] top-[-35%] w-[190%] h-[45%] skew-x-[15deg] skew-y-[10deg]">
+        </data>
+        <div className="absolute top-0 left-0 max-w-screen max-h-screen overflow-hidden w-full h-full opacity-50">
+          <div className="absolute top-0 left-0 w-3/4 max-w-6xl object-contain">
             <img
-              src={graph}
-              alt="logo"
-              className="absolute top-2/6 right-2/4 w-full h-full z-[5]"
+              src={world_bg}
+              alt="world_bg"
+              className="w-full h-full max-w-screen-xl max-h-screen-2xl object-cover"
             />
-            <div className="absolute top-[42%] right-[62%] w-[90%] h-[90%] bg-beergam-blue-plano z-[3] rounded-2xl opacity-50"></div>
           </div>
-          <div className="absolute right-2/5 top-[10%] w-[85%] aspect-square max-w-full h-[35%] skew-x-[15deg] skew-y-[10deg]">
-            <img
-              src={calendar}
-              alt="logo"
-              className="absolute top-2/6 right-2/4 w-full h-full z-[5]"
-            />
-            <div className="absolute top-[42%] right-[62%] w-[90%] h-[90%] bg-beergam-blue-plano z-[3] rounded-2xl opacity-50"></div>
-          </div>
+          <div className="absolute max-h-[60rem] opacity-0 max-w-80 w-[16%] h-10/12 right-0 top-1/2 -translate-y-1/2 sm:opacity-100">
+            <div className="absolute right-[-30%] top-[-35%] w-[190%] h-[45%] skew-x-[15deg] skew-y-[10deg]">
+              <img
+                src={graph}
+                alt="logo"
+                className="absolute top-2/6 right-2/4 w-full h-full z-[5]"
+              />
+              <div className="absolute top-[42%] right-[62%] w-[90%] h-[90%] bg-beergam-blue-plano z-[3] rounded-2xl opacity-50"></div>
+            </div>
+            <div className="absolute right-2/5 top-[10%] w-[85%] aspect-square max-w-full h-[35%] skew-x-[15deg] skew-y-[10deg]">
+              <img
+                src={calendar}
+                alt="logo"
+                className="absolute top-2/6 right-2/4 w-full h-full z-[5]"
+              />
+              <div className="absolute top-[42%] right-[62%] w-[90%] h-[90%] bg-beergam-blue-plano z-[3] rounded-2xl opacity-50"></div>
+            </div>
 
-          <div className="absolute right-[90%] bottom-[-10%] w-[85%] max-w-44 h-[55%] flex flex-col gap-4 skew-x-[15deg] skew-y-[10deg]">
-            <CardComponent title="Impressões" value="126.1k" />
-            <CardComponent title="Cliques" value="4.4k" />
-            <CardComponent title="CTR" value="3,47%" />
-          </div>
-          <div className="absolute right-[35%] bottom-[8%] w-[55%] max-w-44 h-[35%] skew-x-[15deg] skew-y-[10deg]">
-            <img src={card} alt="" />
+            <div className="absolute right-[90%] bottom-[-10%] w-[85%] max-w-44 h-[55%] flex flex-col gap-4 skew-x-[15deg] skew-y-[10deg]">
+              <CardComponent title="Impressões" value="126.1k" />
+              <CardComponent title="Cliques" value="4.4k" />
+              <CardComponent title="CTR" value="3,47%" />
+            </div>
+            <div className="absolute right-[35%] bottom-[8%] w-[55%] max-w-44 h-[35%] skew-x-[15deg] skew-y-[10deg]">
+              <img src={card} alt="" />
+            </div>
           </div>
         </div>
+        <div
+          className={`w-full h-full z-50 ${pageType == "login" ? "flex items-center justify-center" : ""}`}
+        >
+          {children}
+        </div>
       </div>
-      <div
-        className={`w-full h-full z-50 ${pageType == "login" ? "flex items-center justify-center" : ""}`}
-      >
-        {children}
-      </div>
-    </div>
+      {init && (
+        <Particles options={options} particlesLoaded={particlesLoaded} />
+      )}
+    </>
   );
 }
--- app/routes/registro/components/FormModal.tsx
+++ app/routes/registro/components/FormModal.tsx
@@ -82,7 +82,7 @@ export default function FormModal() {
     return true;
   }
   return (
-    <div className="h-full bg-beergam-white p-8 rounded-xl gap-4 flex flex-col">
+    <div className="h-full shadow-lg/55 bg-beergam-white p-8 rounded-xl gap-4 flex flex-col">
       <h1 className="text-beergam-blue-primary">Cadastre-se</h1>
       <div>
         <Fields.wrapper>
