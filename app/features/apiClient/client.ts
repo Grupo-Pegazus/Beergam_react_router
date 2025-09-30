78b09965acb8de637846893155aeefd31dc817eb
Pegdevcolab02 <pegdevcolab02@gmail.com>
2025-09-30 16:59:34 -0300

    feat(apiClient): adicionando interceptor no axios para verificar se o token do usuário está inválido

--- app/features/apiClient/client.ts
+++ app/features/apiClient/client.ts
@@ -157,13 +157,65 @@ export class TypedApiClient {
 }
 
 // Instância tipada do cliente
+
 const typedApiClient = new TypedApiClient("http://localhost:5000", {
   headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true,
 });
+let refreshPromise: Promise<ApiResponse<unknown>> | null = null;
+typedApiClient.axiosInstance.interceptors.response.use(
+  async (response) => {
+    console.log("response do interceptor", response);
+    return response;
+  },
+  async (error) => {
+    const original = error.config;
+    if (error.response?.status === 401) {
+      if (error.response?.data?.error_code === 1002 && !original._retry) {
+        //Verifica se o erro é de access_token expirado e se não foi feito um refresh ainda
+        original._retry = true;
+        if (!refreshPromise) {
+          refreshPromise = typedApiClient
+            .post("/v1/auth/refresh")
+            .finally(() => {
+              refreshPromise = null;
+            });
+        }
+        await refreshPromise;
+        return typedApiClient.axiosInstance(original);
+      }
+      if (error.response?.data?.error_code === 1000) {
+        localStorage.removeItem("userInfo");
+        localStorage.removeItem("userInfoIV");
+        window.location.href = "/login";
+      }
+    }
+    // console.log("erro do interceptor", error);
 
+    // if (!error.response.data) {
+    //   return Promise.reject(error);
+    // }
+    // if (error.response.data.error_code === 1002) {
+    //   console.error("Erro de access_token expirado");
+    //   const response = await typedApiClient.post("/v1/auth/refresh");
+    //   if (response.success) {
+    //     console.log("access_token atualizado");
+    //     window.location.reload();
+    //     return response;
+    //   } else {
+    //     console.error("Erro ao atualizar access_token");
+    //   }
+    // }
+
+    return Promise.reject(error);
+  }
+);
+typedApiClient.axiosInstance.interceptors.request.use(async (config) => {
+  console.log("request do interceptor", config);
+  return config;
+});
 // typedApiClient.axiosInstance.interceptors.response.use(async (response) => {
 //   return response;
 // });
