9e5ab97e2bd5fc8ce77264fcb200ddfff2fb09aa
Pegdevcolab02 <pegdevcolab02@gmail.com>
2025-10-01 12:59:29 -0300

    feat: colocando o componente de hint

---
+++ app/src/components/utils/Hint/index.tsx
@@ -0,0 +1,22 @@
+import Svg from "~/src/assets/svgs";
+
+export default function Hint({
+  message,
+  isCusto,
+}: {
+  message: string;
+  isCusto?: boolean;
+}) {
+  return (
+    <div
+      className={`group relative flex w-fit cursor-pointer ${isCusto ? "text-red-500" : ""}`}
+    >
+      <div>
+        <Svg.information_circle />
+      </div>
+      <div className="invisible absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-[170%] whitespace-nowrap rounded-md bg-[#333] px-2.5 py-1.5 text-xs text-white opacity-0 transition-all duration-300 ease-in-out group-hover:visible group-hover:opacity-100">
+        {message}
+      </div>
+    </div>
+  );
+}
