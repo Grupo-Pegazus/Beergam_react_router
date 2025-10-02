6104949bc159804f2718de4e251250ef66f5fa02
pegdevColab <pegdevColab@gmail.com>
2025-10-02 17:51:37 -0300

    feat(log): adicionando monitoramento do sentry e já preparando ambiente para se adaptar a linux para produção

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,6 +1,16 @@
+import * as Sentry from "@sentry/react-router";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: "https://d60970e1a513659438f62932021d473b@o4508965755158528.ingest.us.sentry.io/4510121563193344",
+    // Adds request headers and IP for users, for more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+    sendDefaultPii: true,
+  });
+}
 
 startTransition(() => {
   hydrateRoot(
