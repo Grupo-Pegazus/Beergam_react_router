df8fb7537817a5f0dddcb2ccd2780ead7adc8653
Yan Batista Bueno <yanbatista2704@gmail.com>
2025-10-20 16:06:26 -0300

    refactor: refazendo lógica de erro visual no input

--- app/src/components/utils/_fields/input.tsx
+++ app/src/components/utils/_fields/input.tsx
@@ -1,32 +1,37 @@
-import { useState, type HTMLInputTypeAttribute } from "react";
+import { useState, type InputHTMLAttributes } from "react";
+import { Tooltip } from "react-tooltip";
 import Svg from "~/src/assets/svgs";
-import { type InputError } from "./typings";
 
-interface InputProps {
-  placeholder?: string;
-  required?: boolean;
-  value: string | number;
-  type?: HTMLInputTypeAttribute;
-  error?: InputError;
-  hasError?: boolean;
-  onChange?: (params: React.ChangeEvent<HTMLInputElement>) => void;
-  onBlur?: (params: React.ChangeEvent<HTMLInputElement>) => void;
-  onFocus?: (params: React.ChangeEvent<HTMLInputElement>) => void;
-  name?: string;
-  style?: React.CSSProperties;
-  min?: number;
-  max?: number;
-  disabled?: boolean | undefined;
+// interface InputProps {
+//   placeholder?: string;
+//   required?: boolean;
+//   value: string | number;
+//   type?: HTMLInputTypeAttribute;
+//   error?: string;
+//   onChange?: (params: React.ChangeEvent<HTMLInputElement>) => void;
+//   onBlur?: (params: React.ChangeEvent<HTMLInputElement>) => void;
+//   onFocus?: (params: React.ChangeEvent<HTMLInputElement>) => void;
+//   name?: string;
+//   style?: React.CSSProperties;
+//   min?: number;
+//   max?: number;
+//   disabled?: boolean | undefined;
+//   tailWindClasses?: string;
+//   dataTooltipId?: string;
+// }
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  error?: string;
+  success?: boolean;
   tailWindClasses?: string;
+  dataTooltipId?: string;
+  showVariables?: boolean;
 }
-
 export default function Input({
   error,
   placeholder,
   required,
   value,
   type,
-  hasError = true,
   onChange,
   onBlur,
   name,
@@ -36,25 +41,48 @@ export default function Input({
   max,
   disabled,
   tailWindClasses,
+  dataTooltipId,
+  success,
+  showVariables,
+  ...props
 }: InputProps) {
-  const isValid = required && value && !error?.error;
+  const isValid = value && (!error || error != "") && success;
 
   const baseClasses =
     type != "checkbox"
       ? "w-full px-3 py-2.5 border border-black/20 rounded text-sm bg-white text-[#1e1f21] transition-colors duration-200 outline-none"
       : "appearance-none w-5 h-5 cursor-pointer border border-black/20 rounded bg-white relative outline-none checked:bg-beergam-orange checked:border-beergam-orange hover:border-beergam-orange after:content-[''] after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:scale-0 after:w-1.5 after:h-2.5 after:border-r-[2.5px] after:border-b-[2.5px] after:border-white after:origin-center after:transition-transform after:duration-200 after:ease-in-out after:opacity-0 checked:after:scale-100 checked:after:rotate-45 checked:after:opacity-100";
-  const errorClasses = error?.error
-    ? "border-red-500 focus:border-red-500"
+  const errorClasses =
+    error && error != ""
+      ? "!border-beergam-red focus:!border-beergam-red/90"
+      : "";
+  const successClasses = isValid
+    ? "!border-beergam-green focus:!border-beergam-green/90"
     : "";
-  const successClasses = isValid ? "border-green-500" : "";
   const focusClasses = "focus:border-[#ff8a00] outline-beergam-orange";
   const disabledClasses = disabled
     ? "bg-gray-50 cursor-not-allowed border-gray-300 text-slate-500"
     : "";
   const [showPassword, setShowPassword] = useState(false);
+  // State para verificar se o usuário está focado ou interagindo com o input
+  const [isInteracting, setIsInteracting] = useState(false);
+
+  // Handlers para foco/interação
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    setIsInteracting(true);
+    if (onFocus) onFocus(e);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setIsInteracting(false);
+    if (onBlur) onBlur(e);
+  };
   return (
     <>
-      <div className="relative w-full flex items-center justify-center flex-col">
+      <div
+        data-tooltip-id={dataTooltipId}
+        className="relative w-full flex items-center justify-center flex-col"
+      >
         <input
           type={
             type === "password" ? (showPassword ? "text" : "password") : type
@@ -64,13 +92,16 @@ export default function Input({
           required={required}
           className={`${baseClasses} ${errorClasses} ${successClasses} ${focusClasses} ${disabledClasses} ${type === "password" ? "pr-10" : ""} ${tailWindClasses}`}
           onChange={onChange}
-          onBlur={onBlur}
-          onFocus={onFocus}
+          onBlur={handleBlur}
+          onFocus={handleFocus}
           name={name}
           style={style}
           min={min}
           max={max}
           disabled={disabled}
+          // onMouseEnter={() => setIsInteracting(true)}
+          // onMouseLeave={() => setIsInteracting(false)}
+          {...props}
         />
         {type === "password" && (
           <button
@@ -88,12 +119,20 @@ export default function Input({
           </button>
         )}
       </div>
-      {hasError && (
-        <p
-          className={`text-xs text-red-500 min-h-5 mt-1 lg:min-h-[16px] 2xl:min-h-5  ${error?.error ? "opacity-100" : "opacity-0"}`}
-        >
-          {error?.message || ""}
-        </p>
+      {dataTooltipId && (
+        <Tooltip
+          id={dataTooltipId}
+          content={error || ""}
+          isOpen={isInteracting}
+          className="z-50"
+        />
+      )}
+      {showVariables && (
+        <div className="">
+          <p>error: {JSON.stringify(error)}</p>
+          <p>success: {JSON.stringify(success)}</p>
+          <p>isValid: {JSON.stringify(isValid)}</p>
+        </div>
       )}
     </>
   );
