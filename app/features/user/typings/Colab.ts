8cb0a5b7e1134b50c99ac3a0b7707d7025a31de1
Yan Batista Bueno <yanbatista2704@gmail.com>
2025-10-24 15:16:23 -0300

    fix: arrumando schema de BaseUser, Colab e User para recebrem as informações certas do backend. Colocando verificações de tipos de usuário na rota de login

--- app/features/user/typings/Colab.ts
+++ app/features/user/typings/Colab.ts
@@ -1,5 +1,9 @@
 import { z } from "zod";
-import { BaseUserSchema, type IBaseUser } from "./BaseUser";
+import {
+  BaseUserDetailsSchema,
+  BaseUserSchema,
+  type IBaseUser,
+} from "./BaseUser";
 export type ColabAction = "Editar" | "Excluir" | "Visualizar";
 export enum ColabLevel {
   ADMIN = "Administrador",
@@ -8,17 +12,23 @@ export enum ColabLevel {
 
 interface IColabDetails {
   level: ColabLevel;
+  photo_id?: string | null;
 }
 
-export const ColabDetailsSchema = z.object({
+export const ColabDetailsSchema = BaseUserDetailsSchema.extend({
   level: z.enum(Object.keys(ColabLevel) as [ColabLevel, ...ColabLevel[]]),
-});
+  photo_id: z.string().optional().nullable(),
+}) satisfies z.ZodType<IColabDetails>;
 
 export interface IColab extends IBaseUser {
   details: IColabDetails;
+  is_online?: boolean;
+  last_online_update?: string | null;
 }
 export const ColabSchema = BaseUserSchema.extend({
   details: ColabDetailsSchema,
+  is_online: z.boolean().optional().default(false),
+  last_online_update: z.string().optional().nullable(),
 }) satisfies z.ZodType<IColab>;
 
 export function FormatColabLevel(level: ColabLevel): keyof typeof ColabLevel {
