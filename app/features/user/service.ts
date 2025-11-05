9a23670426dccf11cc7952b550f8933dcc58c068
Yan Batista Bueno <yanbatista2704@gmail.com>
2025-11-05 13:12:05 -0300

    refactor: refatorando lógica de tipagem de colaborador

--- app/features/user/service.ts
+++ app/features/user/service.ts
@@ -1,5 +1,6 @@
 import { typedApiClient } from "../apiClient/client";
 import type { ApiResponse } from "../apiClient/typings";
+import type { IColab } from "./typings/Colab";
 import type { IUser } from "./typings/User";
 
 class UserService {
@@ -17,6 +18,53 @@ class UserService {
         data: {} as IUser,
         message:
           "Erro ao editar informações do usuário. Tente novamente em alguns instantes.",
+        error_code: 500,
+        error_fields: {},
+      };
+    }
+  }
+  async updateColab(editColab: IColab): Promise<ApiResponse<IColab>> {
+    const colabPin = editColab.pin;
+    if (!colabPin) {
+      return {
+        success: false,
+        data: {} as IColab,
+        message: "Pin do colaborador não encontrado.",
+        error_code: 400,
+        error_fields: {},
+      };
+    }
+    try {
+      const response = await typedApiClient.patch<IColab>(
+        `/v1/users/me/colabs/${colabPin}`,
+        editColab
+      );
+      return response;
+    } catch (error) {
+      console.error("error do editColab", error);
+      return {
+        success: false,
+        data: {} as IColab,
+        message:
+          "Erro ao editar informações do colaborador. Tente novamente em alguns instantes.",
+        error_code: 500,
+        error_fields: {},
+      };
+    }
+  }
+  async getColabs(): Promise<ApiResponse<IColab[]>> {
+    try {
+      const response = await typedApiClient.get<IColab[]>(
+        "/v1/users/me/colabs"
+      );
+      return response;
+    } catch (error) {
+      console.error("error do getColabs", error);
+      return {
+        success: false,
+        data: [],
+        message:
+          "Erro ao buscar colaboradores. Tente novamente em alguns instantes.",
         error_code: 500,
         error_fields: {},
       };
