976ac8d3de97d8954e9fd83d90b0a07633d938ed
Matheus Eduardo <matheus081105@gmail.com>
2025-11-11 17:05:10 -0300

    fix: arrumando lógica q tinha feito errado, burro dms

--- app/routes/inicio/route.tsx
+++ app/routes/inicio/route.tsx
@@ -1,9 +1,5 @@
 import type { Route } from ".react-router/types/app/routes/inicio/+types/route";
-import Reputacao from "~/features/metricsAccount/components/Reputacao/Reputacao";
-import ScheduleTimes from "~/features/metricsAccount/components/ScheduleTimes/ScheduleTimes";
-import Section from "~/src/components/ui/Section";
-import Grid from "~/src/components/ui/Grid";
-import Visitas from "~/features/metricsAccount/components/Visitas/Visitas";
+import InicioPage from "./page";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,18 +10,6 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Inicio() {
   return (
-    <>
-      <Section title="Geral da conta">
-        <Grid cols={{ base: 1, sm: 2, md: 3, lg: 4 }}>
-          <Reputacao />
-          <ScheduleTimes />
-        </Grid>
-      </Section>
-      <Section title="Visitas na conta">
-        <Grid cols={{ base: 1 }}>
-          <Visitas />
-        </Grid>
-      </Section>
-    </>
+    <InicioPage />
   );
 }
