116daf5f21d5b073bcb7d2183f49570f9a161783
Yan Batista Bueno <yanbatista2704@gmail.com>
2025-11-12 13:06:09 -0300

    style: arrumando design do menu e header

--- app/features/system/components/desktop/MenuDesktop.tsx
+++ app/features/system/components/desktop/MenuDesktop.tsx
@@ -1,19 +1,17 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo } from "react";
 import { useSelector } from "react-redux";
+import beergam_flower_logo from "~/src/img/beergam_flower_logo.webp";
 import { type RootState } from "~/store";
 import MenuItem from "../../../menu/components/MenuItem/MenuItem";
-import { MenuProvider } from "../../../menu/context/MenuContext";
 import { useActiveMenu } from "../../../menu/hooks";
 import { useMenuActions } from "../../../menu/hooks/useMenuActions";
 import { useMenuState } from "../../../menu/hooks/useMenuState";
 import { MenuHandler, type MenuState } from "../../../menu/typings";
-
 function MenuDesktopContent() {
   useActiveMenu(MenuHandler.getMenu());
   const user = useSelector((state: RootState) => state.user);
-  const { closeMany } = useMenuActions();
-  const { openKeys } = useMenuState();
-  const [isExpanded, setIsExpanded] = useState(false);
+  const { closeMany, setIsExpanded } = useMenuActions();
+  const { openKeys, isExpanded } = useMenuState();
 
   const handleMouseLeave = useCallback(() => {
     if (openKeys.length > 0) {
@@ -46,9 +44,22 @@ function MenuDesktopContent() {
         onMouseEnter={() => {
           setIsExpanded(true);
         }}
-        className="group fixed top-14 z-1001 h-[calc(100vh-56px)] w-[100px] hover:w-[267px] transition-all duration-200 text-white bg-beergam-blue-primary flex flex-col gap-5 py-2"
+        className="group fixed top-2 z-99999 h-screen w-[100px] hover:w-[267px] transition-all duration-200 text-white bg-beergam-blue-primary flex flex-col gap-5 py-2"
       >
-        <div className="px-[18px]"></div>
+        <div className="px-[18px] flex items-end justify-start">
+          <div className="w-full max-w-[64px] px-4">
+            <img
+              src={beergam_flower_logo}
+              alt="Beergam"
+              className="w-full h-full max-h-full max-w-full object-contain"
+            />
+          </div>
+          <span
+            className={`absolute left-[68px] text-[18px] font-bold text-beergam-white ${isExpanded ? "opacity-100 translate-x-0" : "opacity-0 translate-x-[-10px]"}`}
+          >
+            eergam
+          </span>
+        </div>
         <ul className="flex flex-col gap-2 list-none max-h-[90%] overflow-y-auto overflow-x-hidden px-[18px] [scrollbar-gutter:stable_both-edges] [&::-webkit-scrollbar]:w-[3px] [&::-webkit-scrollbar-thumb]:bg-[#b8c0c2] [&::-webkit-scrollbar-thumb]:rounded-[10px]">
           {Object.entries(menu).map(([key, item]) => (
             <MenuItem key={key} item={item} itemKey={key} parentKey="" />
@@ -61,9 +72,5 @@ function MenuDesktopContent() {
 }
 
 export default function MenuDesktop() {
-  return (
-    <MenuProvider>
-      <MenuDesktopContent />
-    </MenuProvider>
-  );
+  return <MenuDesktopContent />;
 }
