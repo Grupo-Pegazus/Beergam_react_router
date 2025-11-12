0a493876aa064d197951b9becf2d0923224223d8
Yan Batista Bueno <yanbatista2704@gmail.com>
2025-11-12 13:30:00 -0300

    feat(menu): colocando pro menu redirecionar caso o usuário não tenha uma conta marketplace selecionada

--- app/features/menu/components/layout/MenuLayout.tsx
+++ app/features/menu/components/layout/MenuLayout.tsx
@@ -1,9 +1,20 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet, useRouteLoaderData } from "react-router";
+import type { BaseMarketPlace } from "~/features/marketplace/typings";
 import MenuDesktop from "~/features/system/components/desktop/MenuDesktop";
 import SystemLayout from "~/features/system/components/layout/SystemLayout";
 import { MenuProvider } from "../../context/MenuContext";
 
 export default function MenuLayout() {
+  const rootData = useRouteLoaderData("root") as
+    | { marketplace?: BaseMarketPlace }
+    | undefined;
+
+  const marketplace = rootData?.marketplace;
+
+  if (!marketplace) {
+    return <Navigate to="/interno/choosen_account" replace />;
+  }
+
   return (
     <MenuProvider>
       <div className="flex bg-(--color-beergam-blue-primary)">
