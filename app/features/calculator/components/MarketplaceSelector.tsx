b3553f48c01e0c7823830a9528f205545380e959
Matheus Eduardo <matheus081105@gmail.com>
2025-11-25 16:02:47 -0300

    criando página de calculadora

---
+++ app/features/calculator/components/MarketplaceSelector.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Fields } from "~/src/components/utils/_fields";
+
+interface MarketplaceSelectorProps {
+  value: "ml" | "shopee";
+  onChange: (value: "ml" | "shopee") => void;
+}
+
+export default function MarketplaceSelector({
+  value,
+  onChange,
+}: MarketplaceSelectorProps) {
+  return (
+    <Fields.wrapper>
+      <Fields.label
+        text="Marketplace"
+        hint="Selecione o marketplace para calcular"
+      />
+      <Fields.select
+        value={value}
+        options={[
+          { value: "ml", label: "Mercado Livre" },
+          { value: "shopee", label: "Shopee" },
+        ]}
+        onChange={(e) => onChange(e.target.value as "ml" | "shopee")}
+      />
+    </Fields.wrapper>
+  );
+}
+
