5562a55e2d767c7b0d113351991630f499d6dd64
Yan Batista Bueno <yanbatista2704@gmail.com>
2025-12-01 16:17:14 -0300

    refactor: colocando para a sessão de Minha Conta utilizar a nova biblioteca e ajustando o componente de UserFields

--- app/features/user/components/UserFields.tsx
+++ app/features/user/components/UserFields.tsx
@@ -1,57 +1,153 @@
-import CopyButton from "~/src/components/ui/CopyButton";
 import { Fields } from "~/src/components/utils/_fields";
-import Hint from "~/src/components/utils/Hint";
-
-export default function UserFields({
-  canAlter,
-  value,
-  label,
-  onChange,
-  type = "input",
-  clipboard = false,
-  hint,
-}: {
-  canAlter: boolean;
-  value: string | null | undefined;
+import type { InputProps } from "~/src/components/utils/_fields/input";
+// export default function UserFields({
+//   canAlter,
+//   value,
+//   label,
+//   onChange,
+//   type = "input",
+//   clipboard = false,
+//   hint,
+//   options,
+//   error,
+//   errorMessage,
+//   dataTooltipId,
+// }: {
+//   canAlter: boolean;
+//   value: string | null | undefined;
+//   label: string;
+//   type?: "input" | "text";
+//   onChange?: (
+//     e:
+//       | React.ChangeEvent<HTMLInputElement>
+//       | React.ChangeEvent<HTMLSelectElement>
+//   ) => void;
+//   clipboard?: boolean;
+//   hint?: string;
+//   options?: { value: string; label: string }[];
+//   error?: boolean;
+//   errorMessage?: string;
+//   dataTooltipId?: string;
+// }) {
+//   let optionsList = options ?? [];
+//   if (options && (value === null || value === undefined || value === "")) {
+//     optionsList = [
+//       { value: null as unknown as string, label: "Selecione um valor" },
+//       ...options,
+//     ];
+//   }
+//   return (
+//     <Fields.wrapper className="w-full!">
+//       <div className="flex items-center gap-1">
+//         <Fields.label text={label} />
+//         {hint && (
+//           <Hint
+//             message={hint}
+//             anchorSelect={label.toLocaleLowerCase() + "-tooltip"}
+//           />
+//         )}
+//       </div>
+//       {type === "input" && !options && (
+//         <Fields.input
+//           value={value ?? ""}
+//           placeholder="Campo não preenchido."
+//           onChange={onChange}
+//           disabled={!canAlter}
+//           clipboard={clipboard}
+//           error={error ? errorMessage : undefined}
+//           dataTooltipId={dataTooltipId}
+//         />
+//       )}
+//       {type === "input" && options && (
+//         <Fields.select
+//           value={value ?? ""}
+//           options={optionsList}
+//           onChange={(e) => onChange?.(e)}
+//           disabled={!canAlter}
+//         />
+//       )}
+//       {type === "text" && (
+//         <div className="flex items-center gap-2">
+//           <p className="text-beergam-blue-primary">
+//             {value ?? "Campo não preenchido."}
+//           </p>
+//           {clipboard && (
+//             <CopyButton
+//               textToCopy={value ?? ""}
+//               iconSize="h-3.5 w-3.5 md:h-4 md:w-4"
+//               ariaLabel="Copiar"
+//             />
+//           )}
+//         </div>
+//       )}
+//     </Fields.wrapper>
+//   );
+// }
+interface UserFieldsProps extends InputProps {
   label: string;
-  type?: "input" | "text";
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  clipboard?: boolean;
   hint?: string;
-}) {
+  access?: boolean;
+  name: string;
+  canAlter: boolean;
+  nullable?: boolean;
+  options?: { value: string; label: string }[];
+}
+export default function UserFields({
+  label,
+  name,
+  error,
+  onChange,
+  canAlter,
+  options,
+  value,
+  nullable = false,
+  ...props
+}: UserFieldsProps) {
+  const selectValue = value
+    ? typeof value === "string"
+      ? value
+      : String(value)
+    : null;
+
+  // Se não tiver options, adiciona a option "Selecione um campo"
+  const listedOption = nullable
+    ? [{ value: "", label: "Selecione um campo" }, ...(options || [])]
+    : options;
   return (
     <Fields.wrapper className="w-full!">
-      <div className="flex items-center gap-1">
-        <Fields.label text={label} />
-        {hint && (
-          <Hint
-            message={hint}
-            anchorSelect={label.toLocaleLowerCase() + "-tooltip"}
+      <Fields.label text={label} />
+      {options ? (
+        <>
+          <Fields.select
+            name={name}
+            value={selectValue}
+            options={listedOption}
+            onChange={(e) => {
+              if (onChange) {
+                // Cria um objeto compatível com o que o register espera
+                const syntheticEvent = {
+                  ...e,
+                  target: {
+                    value: e.target.value,
+                    name: e.target.name,
+                  },
+                } as unknown as React.ChangeEvent<HTMLInputElement>;
+                onChange(syntheticEvent);
+              }
+            }}
+            disabled={canAlter ? false : true}
+            error={error ? { message: error, error: true } : undefined}
           />
-        )}
-      </div>
-      {type === "input" && (
+        </>
+      ) : (
         <Fields.input
-          value={value ?? ""}
-          placeholder="Campo não preenchido."
+          name={name}
+          disabled={canAlter ? false : true}
           onChange={onChange}
-          disabled={!canAlter}
-          clipboard={clipboard}
+          error={error}
+          dataTooltipId={`${name}-input`}
+          {...props}
         />
-      )}
-      {type === "text" && (
-        <div className="flex items-center gap-2">
-          <p className="text-beergam-blue-primary">
-            {value ?? "Campo não preenchido."}
-          </p>
-          {clipboard && (
-            <CopyButton
-              textToCopy={value ?? ""}
-              iconSize="h-3.5 w-3.5 md:h-4 md:w-4"
-              ariaLabel="Copiar"
-            />
-          )}
-        </div>
       )}
     </Fields.wrapper>
   );
--- app/routes/config/components/MinhaConta/index.tsx
+++ app/routes/config/components/MinhaConta/index.tsx
@@ -1,22 +1,56 @@
 import { useMutation } from "@tanstack/react-query";
-import dayjs from "dayjs";
+import { useReducer } from "react";
+// import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Switch } from "@mui/material";
+import { useForm, type Resolver } from "react-hook-form";
+import toast from "react-hot-toast";
+import { z } from "zod";
 import {
   MenuConfig,
   type MenuKeys,
   type MenuState,
 } from "~/features/menu/typings";
-import AllowedTimes from "~/features/user/colab/components/ColabDetails/AllowedTimes";
-import AllowedViews from "~/features/user/colab/components/ColabDetails/AllowedViews";
-import ColabPhoto from "~/features/user/colab/components/ColabPhoto";
+import UserFields from "~/features/user/components/UserFields";
 import { userService } from "~/features/user/service";
 import type { IColab } from "~/features/user/typings/Colab";
-import { ColabLevel } from "~/features/user/typings/Colab";
-import type { IUser } from "~/features/user/typings/User";
-import { FormatColabLevel, isColab, isMaster } from "~/features/user/utils";
+import {
+  CalcProfitProduct,
+  CalcTax,
+  CurrentBilling,
+  NumberOfEmployees,
+  ProfitRange,
+  UserSchema,
+  type IUser,
+} from "~/features/user/typings/User";
 import Section from "~/src/components/ui/Section";
 import BeergamButton from "~/src/components/utils/BeergamButton";
-import UserFields from "../../../../features/user/components/UserFields";
+import { Fields } from "~/src/components/utils/_fields";
 export default function MinhaConta({ user }: { user: IUser | IColab }) {
+  type editUserFormData = z.infer<typeof UserSchema>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isValid },
+  } = useForm<editUserFormData>({
+    resolver: zodResolver(UserSchema) as unknown as Resolver<editUserFormData>,
+    defaultValues: user as editUserFormData,
+    mode: "onSubmit",
+    reValidateMode: "onChange",
+  });
+  const [editedUser, setEditedUser] = useReducer(
+    (
+      state: typeof user,
+      action: { type: "update"; payload: Partial<typeof user> }
+    ) => {
+      if (!state) return state;
+      return { ...state, ...action.payload } as typeof user;
+    },
+    user as typeof user
+  );
+  const editedUserValidation = UserSchema.safeParse(editedUser);
+
   const allowedViews = user.details.allowed_views ?? ({} as MenuState);
   const accessList = Object.keys(MenuConfig).map((key) => ({
     key,
@@ -26,132 +60,279 @@ export default function MinhaConta({ user }: { user: IUser | IColab }) {
   const colabMutation = useMutation({
     mutationFn: () => userService.getColabs(),
   });
+  const handleChange = (payload: Partial<typeof user>) => {
+    setEditedUser({ type: "update", payload });
+  };
+  const onSubmit = (data: editUserFormData) => {
+    toast.success("Informações do usuário editadas com sucesso");
+    console.log(data);
+  };
+
   return (
     <>
-      {isMaster(user) && (
+      {/* {isMaster(editedUser) && isMaster(user) && (
         <>
+          <p>{JSON.stringify(editedUserValidation.success)}</p>
           <Section
             title="Dados Pessoais"
             className="bg-beergam-white"
             actions={<BeergamButton title="Salvar" />}
           >
             <>
-              <div className="grid grid-cols-2 gap-4">
-                <UserFields
-                  canAlter={false}
-                  value={user.name}
-                  label="Nome"
-                  onChange={() => {}}
-                />
+              <div className="grid grid-cols-3 gap-4">
+                <Fields.input {...register("name")} />
                 <>
-                  <UserFields
-                    canAlter={false}
-                    value={user.details.email}
-                    label="Email"
-                    onChange={() => {}}
-                    clipboard={true}
-                  />
-                  <UserFields
-                    canAlter={user.details.cpf ? false : true}
-                    value={user.details.cpf}
-                    label="CPF"
-                    onChange={() => {}}
-                  />
-                  <UserFields
-                    canAlter={user.details.cnpj ? false : true}
-                    value={user.details.cnpj}
-                    label="CNPJ"
-                    onChange={() => {}}
-                  />
-                  <UserFields
-                    canAlter={false}
-                    value={user.details.phone}
-                    label="Telefone"
-                    onChange={() => {}}
-                  />
-                  <UserFields
-                    canAlter={true}
-                    value={user.details.secondary_phone}
-                    label="WhatsApp"
-                    onChange={() => {}}
-                  />
+                  <Fields.input {...register("name")} />
+                  <Fields.input {...register("name")} />
+                  <Fields.input {...register("name")} />
+                  <Fields.input {...register("name")} />
+                  <Fields.input {...register("name")} />
                 </>
               </div>
               <div className="grid grid-cols-3 mt-4">
-                <UserFields
-                  canAlter={false}
-                  value={user.pin}
-                  label="PIN"
-                  type="text"
-                  hint="O PIN é usado na aba de colaboradores para acessar o sistema."
-                  clipboard
-                />
-                <UserFields
-                  canAlter={false}
-                  value={user.details.personal_reference_code}
-                  label="Código de Referência"
-                  type="text"
-                  hint="O código de referência é usado para convidar novos usuários para o Beergam."
-                  clipboard
-                />
-                <UserFields
-                  canAlter={false}
-                  value={user.details.sub_count?.toString() ?? "0"}
-                  label="Usuários convidados"
-                  type="text"
-                  hint="Compartilhe seu código de referência com parceiros para que eles tenham 14 dias grátis."
-                />
+                <Fields.input {...register("name")} />
+                <Fields.input {...register("name")} />
+                <Fields.input {...register("name")} />
               </div>
             </>
           </Section>
+          <Section title="Dados Financeiros" className="bg-beergam-white">
+            <div className="grid grid-cols-3 gap-4">
+              <Fields.input {...register("name")} />
+              <Fields.input {...register("name")} />
+              <Fields.input {...register("name")} />
+              <Fields.input {...register("name")} />
+              <Fields.input {...register("name")} />
+              <Fields.input {...register("name")} />
+              <Fields.input {...register("name")} />
+              <Fields.input {...register("name")} />
+              <Fields.input {...register("name")} />
+              <Fields.input {...register("name")} />
+              <Fields.input {...register("name")} />
+              <Fields.wrapper>
+                <Fields.label text="Emite nota fiscal em flex" />
+                <Switch
+                  checked={editedUser.details.invoice_in_flex ?? false}
+                  onChange={() => {}}
+                />
+              </Fields.wrapper>
+            </div>
+          </Section>
         </>
       )}
-      {isColab(user) && (
+      {isColab(editedUser) && isColab(user) && (
         <>
           <Section title="Dados do Colaborador" className="bg-beergam-white">
             <div className="grid grid-cols-3 gap-4">
               <div className="flex items-start gap-2">
                 <ColabPhoto
-                  photo_id={user.details.photo_id}
-                  name={user.name}
-                  masterPin={user.master_pin}
+                  photo_id={editedUser.details.photo_id}
+                  name={editedUser.name}
+                  masterPin={editedUser.master_pin}
                   size="large"
                 />
-                <UserFields
-                  canAlter={false}
-                  value={user.name}
-                  label="Nome do Colaborador"
-                  onChange={() => {}}
-                />
+                <Fields.input {...register("name")} />
               </div>
 
-              <UserFields
-                canAlter={false}
-                value={FormatColabLevel(
-                  user.details.level as unknown as ColabLevel
-                )}
-                label="Nível do Colaborador"
-                onChange={() => {}}
-              />
-              <UserFields
-                canAlter={false}
-                value={dayjs(user.updated_at).format("DD/MM/YYYY HH:mm")}
-                label="Data de atualização"
-                onChange={() => {}}
-              />
+              <Fields.input {...register("name")} />
+              <Fields.input {...register("name")} />
             </div>
           </Section>
           <Section
             title="Horários de funcionamento"
             className="bg-beergam-white"
           >
-            <AllowedTimes schedule={user.details.allowed_times} />
+            <AllowedTimes schedule={editedUser.details.allowed_times} />
           </Section>
           <Section title="Acessos Permitidos" className="bg-beergam-white">
             <AllowedViews accessList={accessList} />
           </Section>
         </>
-      )}
+      )} */}
+      <form
+        onSubmit={handleSubmit(onSubmit, (errors) => {
+          toast.error("Você possui erros pendentes no formulário.");
+          console.log("Erros de validação:", errors);
+        })}
+      >
+        <Section
+          title="Dados Pessoais"
+          className="bg-beergam-white"
+          actions={<BeergamButton title="Salvar" type="submit" />}
+        >
+          <div className="grid grid-cols-3 gap-4">
+            <UserFields
+              label="Nome"
+              {...register("name")}
+              error={errors.name?.message}
+              canAlter={false}
+            />
+            <UserFields
+              label="E-mail"
+              {...register("details.email")}
+              error={errors.details?.email?.message}
+              canAlter={false}
+            />
+            <UserFields
+              label="CPF"
+              {...register("details.cpf")}
+              error={errors.details?.cpf?.message}
+              canAlter={user.details?.cpf ? false : true}
+            />
+            <UserFields
+              label="CNPJ"
+              {...register("details.cnpj")}
+              error={errors.details?.cnpj?.message}
+              canAlter={user.details?.cnpj ? false : true}
+            />
+            <UserFields
+              label="Telefone"
+              {...register("details.phone")}
+              error={errors.details?.phone?.message}
+              canAlter={user.details?.phone ? false : true}
+            />
+            <UserFields
+              label="Telefone Secundário"
+              {...register("details.secondary_phone")}
+              error={errors.details?.secondary_phone?.message}
+              canAlter={user.details?.secondary_phone ? false : true}
+            />
+            <UserFields
+              label="PIN"
+              {...register("pin")}
+              error={errors.pin?.message}
+              canAlter={false}
+              clipboard={true}
+            />
+            <UserFields
+              label="Código de Referência"
+              {...register("details.personal_reference_code")}
+              error={errors.details?.personal_reference_code?.message}
+              canAlter={false}
+              clipboard={true}
+            />
+            <UserFields
+              label="Usuários Indicados"
+              {...register("sub_count")}
+              error={errors.sub_count?.message}
+              canAlter={false}
+            />
+          </div>
+        </Section>
+        <Section
+          title="Dados Financeiros"
+          className="bg-beergam-white"
+          actions={<BeergamButton title="Salvar" type="submit" />}
+        >
+          <div className="grid grid-cols-3 gap-4">
+            <UserFields
+              label="Faturamento Mensal"
+              {...register("details.profit_range")}
+              value={watch("details.profit_range") ?? ""}
+              error={errors.details?.profit_range?.message}
+              canAlter={true}
+              options={Object.keys(ProfitRange).map((key) => ({
+                value: key,
+                label: ProfitRange[key as keyof typeof ProfitRange],
+              }))}
+            />
+            <UserFields
+              label="Cálculo de Lucro do Produto"
+              {...register("details.calc_profit_product")}
+              value={watch("details.calc_profit_product") ?? ""}
+              error={errors.details?.calc_profit_product?.message}
+              nullable
+              canAlter={true}
+              options={Object.keys(CalcProfitProduct).map((key) => ({
+                value: key,
+                label: CalcProfitProduct[key as keyof typeof CalcProfitProduct],
+              }))}
+            />
+            <UserFields
+              label="Cálculo de Imposto"
+              {...register("details.calc_tax")}
+              value={watch("details.calc_tax") ?? ""}
+              error={errors.details?.calc_tax?.message}
+              nullable
+              canAlter={true}
+              options={Object.keys(CalcTax).map((key) => ({
+                value: key,
+                label: CalcTax[key as keyof typeof CalcTax],
+              }))}
+            />
+            <UserFields
+              label="Cálculo Fixo de Imposto"
+              {...register("details.tax_percent_fixed")}
+              error={errors.details?.tax_percent_fixed?.message}
+              canAlter={true}
+            />
+            <UserFields
+              label="Faturador Atual"
+              {...register("details.current_billing")}
+              value={watch("details.current_billing") ?? ""}
+              error={errors.details?.current_billing?.message}
+              canAlter={true}
+              nullable
+              options={Object.keys(CurrentBilling).map((key) => ({
+                value: key,
+                label: CurrentBilling[key as keyof typeof CurrentBilling],
+              }))}
+            />
+            <UserFields
+              label="Número de Empregados"
+              {...register("details.number_of_employees")}
+              value={watch("details.number_of_employees") ?? ""}
+              error={errors.details?.number_of_employees?.message}
+              canAlter={true}
+              nullable
+              options={Object.keys(NumberOfEmployees).map((key) => ({
+                value: key,
+                label: NumberOfEmployees[key as keyof typeof NumberOfEmployees],
+              }))}
+            />
+            <UserFields
+              label="Vendas no Mercado Livre"
+              {...register("details.sells_meli")}
+              value={watch("details.sells_meli") ?? ""}
+              error={errors.details?.sells_meli?.message}
+              canAlter={true}
+            />
+            <UserFields
+              label="Vendas no Shopee"
+              {...register("details.sells_shopee")}
+              value={watch("details.sells_shopee") ?? ""}
+              error={errors.details?.sells_shopee?.message}
+              canAlter={true}
+            />
+            <UserFields
+              label="Vendas no Amazon"
+              {...register("details.sells_amazon")}
+              value={watch("details.sells_amazon") ?? ""}
+              error={errors.details?.sells_amazon?.message}
+              canAlter={true}
+            />
+            <UserFields
+              label="Vendas no Shein"
+              {...register("details.sells_shein")}
+              value={watch("details.sells_shein") ?? ""}
+              error={errors.details?.sells_shein?.message}
+              canAlter={true}
+            />
+            <UserFields
+              label="Vendas no Site Próprio"
+              {...register("details.sells_own_site")}
+              value={watch("details.sells_own_site") ?? ""}
+              error={errors.details?.sells_own_site?.message}
+              canAlter={true}
+            />
+            <Fields.wrapper>
+              <Fields.label text="Emite Nota Fiscal em Flex" />
+              <Switch {...register("details.invoice_in_flex")} />
+            </Fields.wrapper>
+          </div>
+        </Section>
+        <p className="mt-2">Form válido: {isValid ? "Sim" : "Não"}</p>
+      </form>
     </>
   );
 }
