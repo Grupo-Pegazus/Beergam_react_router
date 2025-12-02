e1c527a74651c428d1341e96d2b151be0abbc6ea
Yan Batista Bueno <yanbatista2704@gmail.com>
2025-12-02 11:02:17 -0300

    feat(apiClient): criando tipagem para os error_fields

--- app/features/apiClient/typings.ts
+++ app/features/apiClient/typings.ts
@@ -1,7 +1,12 @@
+interface IErrorField<T = unknown> {
+  error: "string";
+  key: "string";
+  value: T;
+}
 export interface ApiResponse<T = unknown> {
   success: boolean;
   data: T;
   message: string;
   error_code?: number;
-  error_fields?: Record<string, string[] | undefined> | undefined;
+  error_fields?: IErrorField[] | undefined;
 }
