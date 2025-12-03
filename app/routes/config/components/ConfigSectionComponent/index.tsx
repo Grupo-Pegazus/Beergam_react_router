3877a60434bfd64c9dbdd7b76043fb31e747ef69
Yan Batista Bueno <yanbatista2704@gmail.com>
2025-12-03 16:00:15 -0300

    style(ConfigSectionComponente): colocando animação de toggle no menu de config

--- app/routes/config/components/ConfigSectionComponent/index.tsx
+++ app/routes/config/components/ConfigSectionComponent/index.tsx
@@ -1,10 +1,14 @@
 export default function ConfigSectionComponent({
   children,
+  menuOpen,
 }: {
   children: React.ReactNode;
+  menuOpen: boolean;
 }) {
   return (
-    <div className="flex flex-col w-full gap-4 items-center max-h-[calc(100vh-70px)] overflow-scroll">
+    <div
+      className={`${menuOpen ? "translate-y-[400px]" : "translate-y-0"} md:translate-y-0! flex flex-col w-full gap-4 items-center max-h-[calc(100vh-70px)] overflow-scroll`}
+    >
       <div className="grid gap-4 md:w-[90%] w-full">{children}</div>
     </div>
   );
