3139cb4d27620d7850f3ec9df99940b5692aa4be
Yan Batista Bueno <yanbatista2704@gmail.com>
2025-12-04 12:06:57 -0300

    refactor(Modal): Colocando lógica do modal para ser um provider

---
+++ app/src/components/utils/Modal/useModal.tsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { ModalContext } from "./ModalContext";
+
+export function useModal() {
+  const context = useContext(ModalContext);
+
+  if (!context) {
+    throw new Error("useModal deve ser usado dentro de um ModalProvider");
+  }
+
+  return context;
+}
+
