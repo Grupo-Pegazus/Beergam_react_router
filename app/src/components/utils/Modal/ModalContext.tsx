7d9618ad5d72e6846726b5c8f021d13fab06637c
Yan Batista Bueno <yanbatista2704@gmail.com>
2025-12-04 16:27:53 -0300

    feat(Modal): colocando onClose no contexto do modal

--- app/src/components/utils/Modal/ModalContext.tsx
+++ app/src/components/utils/Modal/ModalContext.tsx
@@ -4,12 +4,14 @@ export interface ModalContextType {
   openModal: (content: React.ReactNode, options?: ModalOptions) => void;
   closeModal: () => void;
   isOpen: boolean;
+  onClose?: () => void;
 }
 
 export interface ModalOptions {
   title?: string;
   className?: string;
   contentClassName?: string;
+  onClose?: () => void;
 }
 
 export const ModalContext = createContext<ModalContextType | undefined>(
