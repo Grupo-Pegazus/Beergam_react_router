e94baa1a9477772c963bc0f5b22c1147d1a045dc
Matheus Eduardo <matheus081105@gmail.com>
2026-01-15 16:35:27 -0300

    Refactor Header components to comment out unused imports and buttons
    - Commented out unused imports in HeaderDesktop and HeaderMobile components.
    - Temporarily disabled button elements in both components for future implementation.

--- app/features/system/components/desktop/HeaderDesktop.tsx
+++ app/features/system/components/desktop/HeaderDesktop.tsx
@@ -1,12 +1,12 @@
-import { Tooltip } from "react-tooltip";
-import Svg from "~/src/assets/svgs/_index";
+// import { Tooltip } from "react-tooltip";
+// import Svg from "~/src/assets/svgs/_index";
 import AccountView from "./AccountView";
 export default function HeaderDesktop() {
   return (
     <header className="hidden md:block fixed top-0 md:relative left-0 right-0 h-14 z-1001 text-white bg-beergam-menu-background">
       <div className="mx-auto px-4 py-[10px] flex items-center gap-3 justify-end">
         <div className="flex items-center justify-center gap-5">
-          <button
+          {/* <button
             type="button"
             aria-label="Notificações"
             data-tooltip-id="header-notifications-tooltip"
@@ -37,7 +37,7 @@ export default function HeaderDesktop() {
           >
             <Svg.question tailWindClasses="size-4.5" />
           </button>
-          <Tooltip id="header-help-tooltip" place="bottom" />
+          <Tooltip id="header-help-tooltip" place="bottom" /> */}
 
           <AccountView expanded={true} />
         </div>
